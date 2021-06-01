2d7e02f558d37925da0583786a2bd0e2fb0783ff
Márcio Poffo <56840850+mpoffo@users.noreply.github.com>
2021-06-01 20:11:26 -0300

    teste

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_typeahead/flutter_typeahead.dart';
-import 'package:http/http.dart' as http;
-import 'dart:convert' as convert;
+
+import 'br.com.sprintfield.oneonone.person/person.dart';
 
 void main() {
   runApp(MyApp());
@@ -61,44 +61,5 @@ class _MyHomePageState extends State<MyHomePage> {
             ),
           ],
         ));
-  }
-}
-
-class PersonService {
-  static Future<Iterable<Person>> getPersons(String query) async {
-    var url = Uri.parse(
-        'https://raw.githubusercontent.com/mpoffo/oneonone-flutter/main/person.json');
-
-    // Await the http get response, then decode the json-formatted response.
-    try {
-      var response = await http.get(url);
-      if (response.statusCode == 200) {
-        var result = List.from(convert.jsonDecode(response.body))
-            .map((e) => Person.fromJson(e))
-            .toList();
-        print(result);
-        return result;
-      } else {
-        print('Request failed with status: ${response.statusCode}.');
-      }
-    } catch (err) {
-      print(err);
-    }
-
-    return List.empty();
-  }
-}
-
-class Person {
-  String name;
-  Person(this.name);
-
-  factory Person.fromJson(dynamic json) {
-    return Person(json['name'] as String);
-  }
-
-  @override
-  String toString() {
-    return this.name;
   }
 }
