628bec503c75f27364e81ea320784c7165b43337
Márcio Poffo <56840850+mpoffo@users.noreply.github.com>
2021-06-02 00:28:01 -0300

    teste

--- build/web/flutter_service_worker.js
+++ build/web/flutter_service_worker.js
@@ -11,8 +11,8 @@ const RESOURCES = {
 "favicon.png": "5dcef449791fa27946b3d35ad8803796",
 "icons/Icon-192.png": "ac9a721a12bbc803b44f645561ecb1e1",
 "icons/Icon-512.png": "96e752610906ba2a93c65f8abe1645f1",
-"index.html": "02885849e2d03bbe5581799b6f431e40",
-"/": "02885849e2d03bbe5581799b6f431e40",
+"index.html": "798bf5df142c4f8606ac3de68e13892c",
+"/": "798bf5df142c4f8606ac3de68e13892c",
 "main.dart.js": "5615d19fed820a5334b2cc13fc54484f",
 "manifest.json": "6722119729ed52729519a1509a0d0aff",
 "version.json": "0bf01a8c019bb1c9c33cb58f5076f511"
